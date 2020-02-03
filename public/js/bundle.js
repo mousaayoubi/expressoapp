777e11681c1b16433a87eaa58f7f8349f4007f70
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-02-03 19:46:07 +0200

    Update files

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -7864,7 +7864,7 @@ __webpack_require__(231);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Expresso = {};
-var baseUrl = 'https://expresso.now.sh/api';
+var baseUrl = 'https://expressoapp.now.sh/api';
 
 Expresso.getEmployees = function () {
   var url = baseUrl + '/employees';
--- src/utils/Expresso.js
+++ src/utils/Expresso.js
@@ -2,7 +2,7 @@ import camelcaseKeys from './camelcase-keys/index';
 import 'whatwg-fetch';
 
 const Expresso = {};
-const baseUrl = 'https://expresso.now.sh/api';
+const baseUrl = 'https://expressoapp.now.sh/api';
 
 Expresso.getEmployees = () => {
   const url = `${baseUrl}/employees`;
